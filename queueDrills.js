5ec5200e61a9b2d9e22cb158ccdc14d65893fe95
Jess Doyle <jessica.e.doyle@gmail.com>
2019-10-04 14:31:45 -0400

    add doubleQueue

--- queueDrills.js
+++ queueDrills.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Queue = require('./queue');
+const DoubleQueue = require('./doubleQueue');
 const { peek, isEmpty, display } = require('./queueFns');
 
 function queueTest () {
@@ -22,3 +23,24 @@ function queueTest () {
     
 }
 // queueTest();
+
+function doubleQueueTest () {
+  const starTrekQ = new DoubleQueue();
+  
+  starTrekQ.enqueue('Kirk');
+  starTrekQ.enqueue('Spock');
+  starTrekQ.enqueue('Uhara');
+  starTrekQ.enqueue('Sulu');
+  starTrekQ.enqueue('Checkov');
+  
+  // display(starTrekQ);
+  // console.log('###############');
+
+  starTrekQ.dequeue();
+  starTrekQ.dequeue();
+
+  // display(starTrekQ);
+  console.log(peek(starTrekQ));
+    
+}
+doubleQueueTest();
